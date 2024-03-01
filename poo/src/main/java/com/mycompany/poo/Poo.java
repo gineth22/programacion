34e4aa7648163791bfb1600ac89b968cb5b55bb5
gineth22 <getrillosp@ufpso.edu.co>
2024-03-01 10:42:13 -0500

    creacion del proyecto

---
+++ poo/src/main/java/com/mycompany/poo/Poo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.poo;
+
+/**
+ *
+ * @author SCIS2PC27
+ */
+public class Poo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
